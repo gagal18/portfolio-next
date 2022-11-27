910a6e2e775fbd2b105ecdeb60db9bf2acd423bc
gagal18 <gagaleskibojan@gmail.com>
2022-11-27 23:15:41 +0100

    fix: title

--- components/Layout/Head.js
+++ components/Layout/Head.js
@@ -13,8 +13,8 @@ const HeadLinks = (props) => {
         <Head>
             <link rel="shortcut icon" href="/favicon.ico" />
             <meta name="theme-color" content="#ffffff" />
-            <title>Bojan&apos s Portfolio</title>
-            <meta property="og:title" content={'Bojan&apos s Portfolio'} />
+            <title>Bojan's Portfolio</title>
+            <meta property="og:title" content={"Bojan's Portfolio"} />
             <meta property="og:url" content={href} />
             <meta property="og:type" content="website" />
         </Head>
